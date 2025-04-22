e1b6a62d4c1bd95f4b7316077dc27b7a784a15b1
Chien Ngu <chienbutter@gmail.com>
2025-04-22 21:43:39 +0700

    'update'

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const router = require('./api/routes/iPhoneRoute');
 router(app); // Register routes by calling the exported function
 
 // Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running at: http://localhost:${port}`);
 });
