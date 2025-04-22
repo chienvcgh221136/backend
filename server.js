7872b0ea10237a2ed44bc7248ae1e01735bbb494
Chien Ngu <chienbutter@gmail.com>
2025-04-22 21:41:19 +0700

    'web'

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(cors({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// MongoDB connection string (local)
-const db = "mongodb+srv://chienpvgch221136:chienpvgch221136@mydbcluster.6kuho.mongodb.net/?retryWrites=true&w=majority&appName=MyDBCLuster";
+// MongoDB connection string 
+const db = "mongodb+srv://chienpvgch221136:Chien1092004%40@mydbcluster.6kuho.mongodb.net/phone-data";
 
 // Connect to MongoDB using Mongoose
 mongoose.connect(db)
@@ -28,7 +28,7 @@ const router = require('./api/routes/iPhoneRoute');
 router(app); // Register routes by calling the exported function
 
 // Start the server
-const port =process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server running at: http://localhost:${port}`);
 });
